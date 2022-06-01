1cc66fc7e8771429e4fc8e4161606bcb452aa72d
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-06-01 23:30:21 +0100

    test: Test base functionalty

--- test/index.ts
+++ test/index.ts
@@ -3,18 +3,10 @@ import { token } from './config.json';
 
 const client = new Client();
 
-client.once('ready', () => console.log(`Logged in as ${client.user!.name}.`));
-client.on('disconnect', () => console.log('Disconnected.'));
-
-client.on('messageCreate', async (message) => {
-	if (message.content !== '!echo') return;
-	await message.reply({ content: 'What should I echo?', isPrivate: true });
-	const collector = message.channel.createMessageCollector({
-		filter: (msg) => msg.authorId === message.authorId,
-		max: 1,
-		time: 30000,
-	});
-	collector.on('collect', (msg) => message.channel.send(msg.content!));
+client.once('ready', () => console.log(`Logged in as ${client.user!.name}`));
+client.on('disconnect', () => console.log('Disconnected from Guilded'));
+client.on('messageCreate', (message) => {
+	if (message.content === 'ping') message.reply('Pong!');
 });
 
 client.login(token);
