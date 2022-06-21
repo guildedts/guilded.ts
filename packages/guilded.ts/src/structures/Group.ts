17735af331576e9fa74306cc09d06b35ce344696
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-06-21 20:42:13 +0100

    feat(Group): add `fetch` method

--- packages/guilded.ts/src/structures/Group.ts
+++ packages/guilded.ts/src/structures/Group.ts
@@ -10,15 +10,31 @@ export class Group extends Base {
 	/**
 	 * @param client The client the group belongs to.
 	 * @param raw The raw data of the group.
+	 * @param cache Whether to cache the group.
 	 */
-	public constructor(client: Client, public readonly raw: { id: string }) {
+	public constructor(
+		client: Client,
+		public readonly raw: { id: string },
+		cache = client.options.cacheGroups ?? true,
+	) {
 		super(client, raw.id);
 		this.members = new GroupMemberManager(this);
+		if (cache) client.groups.cache.set(this.id, this);
 	}
 
 	/** Whether the group is cached. */
 	public get isCached() {
 		return this.client.groups.cache.has(this.id);
+	}
+
+	/**
+	 * Fetch the group.
+	 * @param cache Whether to cache the fetched group.
+	 * @returns The fetched group.
+	 */
+	public fetch(cache?: boolean) {
+		this.client.groups.cache.delete(this.id);
+		return this.client.groups.fetch(this.id, cache) as this;
 	}
 
 	/**
