e561c38d5f659d977859169dbd2b87a37321373b
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-06-21 20:40:26 +0100

    feat(ServerBan): add `fetchAuthor` method

--- packages/guilded.ts/src/structures/server/ServerBan.ts
+++ packages/guilded.ts/src/structures/server/ServerBan.ts
@@ -17,23 +17,24 @@ export class ServerBan extends Base {
 	/**
 	 * @param server The server the ban belongs to.
 	 * @param raw The raw data of the server ban.
+	 * @param cache Whether to cache the server ban.
 	 */
-	public constructor(public readonly server: Server, public readonly raw: APIServerBan) {
+	public constructor(
+		public readonly server: Server,
+		public readonly raw: APIServerBan,
+		cache = server.client.options.cacheServerBans ?? true,
+	) {
 		super(server.client, raw.user.id);
 		this.user = new User(server.client, raw.user);
 		this.reason = raw.reason;
 		this.createdBy = raw.createdBy;
 		this.createdAt = new Date(raw.createdAt);
+		if (cache) server.bans.cache.set(this.id, this);
 	}
 
 	/** Whether the server ban is cached. */
 	public get isCached() {
 		return this.server.bans.cache.has(this.id);
-	}
-
-	/** The server member the ban belongs to. */
-	public get member() {
-		return this.server.members.cache.get(this.id);
 	}
 
 	/** The user that created the server ban. */
@@ -54,5 +55,14 @@ export class ServerBan extends Base {
 	public fetch(cache?: boolean) {
 		this.server.bans.cache.delete(this.id);
 		return this.server.bans.fetch(this.id, cache) as Promise<this>;
+	}
+
+	/**
+	 * Fetch the server member that created the ban.
+	 * @param cache Whether to cache the fetched server member.
+	 * @returns The fetched server member.
+	 */
+	public async fetchAuthor(cache?: boolean) {
+		return this.server.members.fetch(this.createdBy, cache);
 	}
 }
