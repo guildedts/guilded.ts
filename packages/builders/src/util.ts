b4063b6d46c62481b02bdc28589c10ba999964a4
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-06-22 18:07:54 +0100

    feat: change `COLORS` to `Colors`

--- packages/builders/src/util.ts
+++ packages/builders/src/util.ts
@@ -7,34 +7,36 @@
 export function resolveColor(color: ColorResolvable) {
 	if (color === 'RANDOM') color = Math.floor(Math.random() * (0xffffff + 1));
 	else if (typeof color === `string`)
-		color = COLORS[color as keyof typeof COLORS] ?? parseInt(color.replace('#', ''), 16);
+		color = Colors[color] ?? parseInt(color.replace('#', ''), 16);
 	else if (Array.isArray(color)) color = (color[0] << 16) + (color[1] << 8) + color[2];
-	return color;
+	return color as number;
 }
 
 /** The color resolvable type. */
 export type ColorResolvable =
 	| `#${string}`
-	| keyof typeof COLORS
+	| keyof typeof Colors
 	| 'RANDOM'
 	| number
 	| [r: number, g: number, b: number];
 
 /** A set of color presets. */
-export const COLORS = {
-	WHITE: 0xffffff,
-	BLACK: 0x000000,
-	GREY: 0x343a40,
-	RED: 0xe03131,
-	PINK: 0xc2255c,
-	GRAPE: 0x9c36b5,
-	VIOLET: 0x6741d9,
-	INDIGO: 0x3b5bdb,
-	BLUE: 0x1971c2,
-	CYAN: 0x0c8599,
-	TEAL: 0x099268,
-	GREEN: 0x2f9e44,
-	LIME: 0x66a80f,
-	YELLOW: 0xfcc419,
-	ORANGE: 0xe8590c,
-} as const;
+export enum Colors {
+	DEFAULT = 0x000000,
+	WHITE = 0xffffff,
+	BLACK = 0x111820,
+	GILDED = 0xF5C400,
+	GREY = 0x36363d,
+	RED = 0xe03131,
+	PINK = 0xc2255c,
+	GRAPE = 0x9c36b5,
+	VIOLET = 0x6741d9,
+	INDIGO = 0x3b5bdb,
+	BLUE = 0x1971c2,
+	CYAN = 0x0c8599,
+	TEAL = 0x099268,
+	GREEN = 0x2f9e44,
+	LIME = 0x66a80f,
+	YELLOW = 0xfcc419,
+	ORANGE = 0xe8590c,
+}
