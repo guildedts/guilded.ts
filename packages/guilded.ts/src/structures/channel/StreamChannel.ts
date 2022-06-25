15aa87e17177845f59bd92d264e5f70db0bbe389
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-06-25 22:55:14 +0100

    feat: remove `StreamChannel` and `VoiceChannel`

--- packages/guilded.ts/src/structures/channel/StreamChannel.ts
+++
@@ -1,7 +0,0 @@
-import { APIChannelType } from 'guilded-api-typings';
-import { ChatChannel } from './ChatChannel';
-
-/** Represents a stream channel on Guilded. */
-export class StreamChannel extends ChatChannel {
-	public declare readonly type: APIChannelType.Stream;
-}
