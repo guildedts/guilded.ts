112578eb5006ac2935cc90b9c8bb313c8a9b17b6
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-07-23 23:04:05 +0100

    feat: release webhook

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,5 +1,9 @@
 import { execSync } from 'child_process';
 import { rmSync } from 'fs';
+import fetch from 'node-fetch';
+
+if (!process.env.GUILDED_WEBHOOK_URL)
+	throw new Error('Missing GUILDED_WEBHOOK_URL environment variable');
 
 execSync('pnpm publish -r --ignore-scripts --report-summary', { stdio: 'inherit' });
 
@@ -7,7 +11,22 @@ const { publishedPackages }: PublishSummary = require('../pnpm-publish-summary.j
 
 console.log(process);
 
-if (publishedPackages.length > 0) execSync('pnpm changeset tag', { stdio: 'inherit' });
+if (publishedPackages.length > 0) {
+	execSync('pnpm changeset tag', { stdio: 'inherit' });
+	const packages = publishedPackages.map(
+		({ name, version }) =>
+			`- [${name}@${version}](https://github.com/guildedts/guilded.ts/releases/tag/${name}@${version})`,
+	);
+	fetch(process.env.GUILDED_WEBHOOK_URL!, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ content: `@everyone New releases out now!\n\n${packages.join('\n')}` }),
+	}).then((req) =>
+		req.ok
+			? console.log('Release webhook sent')
+			: console.log('Failed to send release webhook'),
+	);
+}
 
 rmSync('pnpm-publish-summary.json');
 
