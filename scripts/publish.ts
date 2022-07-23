aab55dc70aa8126aa3282016316b7da4bb91668e
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-07-23 22:22:30 +0100

    test

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,8 +3,24 @@ import { rmSync } from 'fs';
 
 execSync('pnpm publish -r --ignore-scripts --report-summary', { stdio: 'inherit' });
 
-const { publishedPackages } = require('../pnpm-publish-summary.json');
+const { publishedPackages }: PublishSummary = require('../pnpm-publish-summary.json');
+
+console.log(process);
 
 if (publishedPackages.length > 0) execSync('pnpm changeset tag', { stdio: 'inherit' });
 
 rmSync('pnpm-publish-summary.json');
+
+/** Represents a publish summary. */
+interface PublishSummary {
+	/** The packages that were published. */
+	publishedPackages: Package[];
+}
+
+/** Represents a package that was published. */
+interface Package {
+	/** The name of the package. */
+	name: string;
+	/** The version of the package. */
+	version: string;
+}
