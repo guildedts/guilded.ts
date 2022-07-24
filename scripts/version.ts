5c9416bd9933a1009d12aa7d4b3c5337b6d112b2
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-07-24 16:36:35 +0100

    feat(scripts): auto deprecate

---
+++ scripts/version.ts
@@ -0,0 +1,17 @@
+import { execSync } from 'child_process';
+import { rmSync } from 'fs';
+
+const dev = process.argv.includes('--dev');
+let versionCommand = 'pnpm changeset version';
+if (dev) versionCommand += ' --snapshot dev';
+
+execSync('pnpm changeset status --output changeset-status.json', { stdio: 'inherit' });
+execSync(versionCommand, { stdio: 'inherit' });
+
+const { releases } = require('../changeset-status.json');
+
+if (dev)
+	for (const { name } of releases)
+		execSync(`pnpm nd -n '*dev*' -p ${name}`, { stdio: 'inherit' });
+
+rmSync('changeset-status.json');
