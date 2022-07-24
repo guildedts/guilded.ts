d20704f352e26f34adcbed42fb92a5f2ebe7caa2
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-07-24 14:33:26 +0100

    feat: dev releases

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,10 +1,14 @@
 import { execSync } from 'child_process';
 import { rmSync } from 'fs';
 
-execSync('pnpm publish -r --ignore-scripts --report-summary', { stdio: 'inherit' });
+const dev = process.argv.includes('--dev');
+let publishCommand = 'pnpm publish -r --ignore-scripts --report-summary';
+if (dev) publishCommand += ' --tag dev --ignore-git-checks';
+
+execSync(publishCommand, { stdio: 'inherit' });
 
 const { publishedPackages } = require('../pnpm-publish-summary.json');
 
-if (publishedPackages.length > 0) execSync('pnpm changeset tag', { stdio: 'inherit' });
+if (publishedPackages.length > 0 && !dev) execSync('pnpm changeset tag', { stdio: 'inherit' });
 
 rmSync('pnpm-publish-summary.json');
