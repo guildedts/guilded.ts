b8b50d706da71e7a05926b81cf1335a64fdf590a
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-07-24 11:38:01 +0100

    feat: remove publish dev

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,14 +1,10 @@
 import { execSync } from 'child_process';
 import { rmSync } from 'fs';
 
-const dev = process.argv.includes('--dev');
-let publishCommand = 'pnpm publish -r --ignore-scripts --report-summary';
-if (dev) publishCommand += ' --tag dev --no-git-checks';
-
-execSync(publishCommand, { stdio: 'inherit' });
+execSync('pnpm publish -r --ignore-scripts --report-summary', { stdio: 'inherit' });
 
 const { publishedPackages } = require('../pnpm-publish-summary.json');
 
-if (publishedPackages.length > 0 && !dev) execSync('pnpm changeset tag', { stdio: 'inherit' });
+if (publishedPackages.length > 0) execSync('pnpm changeset tag', { stdio: 'inherit' });
 
 rmSync('pnpm-publish-summary.json');
