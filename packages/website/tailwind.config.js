dc733953c3a03c0d796976525fed77fe1c9e1fd3
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-10-03 22:05:52 +0100

    feat: add colours

--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -1,4 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['src/**/*.tsx'],
+	theme: {
+		extend: {
+			colors: {
+				guilded: {
+					gilded: '#F5C400',
+					grey: {
+						lightest: '#373943',
+						light: '#32343d',
+						dark: '#292b32',
+						darkest: '#1e2025',
+					},
+				},
+			},
+		},
+	},
 };
