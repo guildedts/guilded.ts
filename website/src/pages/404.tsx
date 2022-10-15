6ec238584d63ba496ed22181f77b4c76c6610aad
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-10-15 15:10:35 +0100

    feat(website): fix 404 page on small screens

--- website/src/pages/404.tsx
+++ website/src/pages/404.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head';
+
 export default function NotFound() {
 	return (
-		<div className="text-white text-5xl h-full flex items-center">
-			<p>Page Not Found</p>
-		</div>
+		<>
+			<Head>
+				<title>Guilded.TS | 404</title>
+			</Head>
+			<div className="text-white text-5xl h-full flex items-center text-center">
+				<p>Page Not Found</p>
+			</div>
+		</>
 	);
 }
